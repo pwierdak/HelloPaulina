b61bd01d65f5989793abc67ea29bb3f65035d243
pwierdak <paulinawierdak95@gmail.com>
2022-09-19 21:05:34 +0200

    Scanner Java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String name=scanner.nextLine();
-        System.out.println("Hello, " +name);
+        System.out.println("Add Value x");
+        int x = scanner.nextInt();
+        if (x<3){
+            System.out.println("X jest mniejsze od 3");
+        }else {
+            System.out.println("X jest wieksze od 3");
+        }
     }
 }
+
